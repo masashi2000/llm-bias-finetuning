95f17cf76658a1bb42a92e71803da39411e9b0df
masashi2000 <s12701350406@gmail.com>
2024-11-12 23:18:13 +0900

    処理を統一したmain_batch.pyで処理するように変えた

--- llama3_1_8b/process.sh
+++ llama3_1_8b/process.sh
@@ -41,41 +41,54 @@ python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agen
 echo "NORMAL CLIMATE CHANGE 1, 1"
 python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/climate_change.txt
 
+######################################
+
 echo "ECHO RACISM 2, 0"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_racism.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_racism.txt --echo_kind individual_level
 
 echo "ECHO RACISM 0, 2"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_racism.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_racism.txt --echo_kind individual_level
+
 echo "ECHO RACISM 1, 1"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_racism.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_racism.txt --echo_kind individual_level
+
 
 # 1つ目のコマンドの実行
 echo "ECHO ILLEGAL IMMIGRATION 2, 0"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_illegal_immigration.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_illegal_immigration.txt --echo_kind individual_level
+
 
 echo "ECHO ILLEGAL IMMIGRATION 0, 2"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_illegal_immigration.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_illegal_immigration.txt --echo_kind individual_level
+
 echo "ECHO ILLEGAL IMMIGRATION 1, 1"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_illegal_immigration.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_illegal_immigration.txt --echo_kind individual_level
+
 
 # 2つ目のコマンドの実行
 echo "ECHO GUN VIOLENCE 2, 0"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_gun_violence.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_gun_violence.txt --echo_kind individual_level
+
 
 echo "ECHO GUN VIOLENCE 0, 2"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_gun_violence.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_gun_violence.txt --echo_kind individual_level
+
 
 echo "ECHO GUN VIOLENCE 1, 1"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_gun_violence.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_gun_violence.txt --echo_kind individual_level
+
 
 # 2つ目のコマンドの実行
 echo "ECHO CLIMATE CHANGE 2, 0"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_climate_change.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 2 --num_republican_agents 0 --instruction_file ./files/echo_climate_change.txt --echo_kind individual_level
+
 
 echo "ECHO CLIMATE CHANGE 0, 2"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_climate_change.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 0 --num_republican_agents 2 --instruction_file ./files/echo_climate_change.txt --echo_kind individual_level
+
 
 echo "ECHO CLIMATE CHANGE 1, 1"
-python main_echo_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_climate_change.txt
+python main_batch.py --trial_times 40 --round_robin_times 10 --num_democrat_agents 1 --num_republican_agents 1 --instruction_file ./files/echo_climate_change.txt --echo_kind individual_level
+
 # 終了メッセージを表示
 echo "Batch scripts completed."
